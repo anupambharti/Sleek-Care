d9d0c03d28bf3262f686aaba8ea1a42df67c435a
Anu <anupambhartiab@gmail.com>
2023-08-29 19:25:56 +0530

    Initial Commit

---
+++ Sleek Care/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sleek Care
+//
+//  Created by Anu on 29/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
