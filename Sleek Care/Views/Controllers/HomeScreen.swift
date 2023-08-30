f54738dfd09378606644045db1f4af0949979b1b
Anu <anupambhartiab@gmail.com>
2023-08-30 21:48:57 +0530

    Database added, Feature to save notes and query them from firestore added

--- Sleek Care/Views/Controllers/HomeScreen.swift
+++ Sleek Care/Views/Controllers/HomeScreen.swift
@@ -6,13 +6,48 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class HomeScreen: UIViewController {
-
+    
+    
+    @IBOutlet weak var addNotesField: UITextField!
+    @IBOutlet weak var notesField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
+    @IBAction func saveNotes(_ sender: Any) {
+        let note = addNotesField.text
+        let db = Firestore.firestore()
+        
+        db.collection("Notes").document("Today").setData([
+            "notes": note
+        ])
+    }
 
+    @IBAction func QueryNotes(_ sender: Any) {
+        FirestoreQuery()
+    }
+    
+    func FirestoreQuery() {
+        let db = Firestore.firestore()
+        let docRef = db.collection("Notes").document("Today")
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                
+                    self.notesField.text = document.get("notes") as! String
+                
+                
+            }
+            
+            else
+            {
+                print("Document does not exist")
+            }
+        }
+    }// Query end
 }
